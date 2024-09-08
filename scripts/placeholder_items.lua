54dc6ee412ad0208273c64bea7a285807fea20f5
Sneglium <faustawker@gmail.com>
2024-09-08 19:11:35 +1200

    update placeholder item appearances

--- scripts/placeholder_items.lua
+++ scripts/placeholder_items.lua
@@ -1,13 +1,12 @@
 
 etc: register_item('icon_time', {
-	inventory_image = 'etc_icon_time.png',
+	inventory_image = 'etc_icon_time.png^etc_icon_bar_overlay.png',
 	groups = {not_in_creative_inventory = 1},
 	stack_max = -1
 })
 
 function etc.get_time_rep_stack (description, seconds)
 	local stack = ItemStack('etcetera:icon_time')
-	stack: set_count(math.max(1, seconds))
 	
 	local minutes = math.floor(seconds / 60)
 	local hours = math.floor(minutes / 60)
@@ -15,14 +14,18 @@ function etc.get_time_rep_stack (description, seconds)
 	minutes = minutes - (hours * 60)
 	new_seconds = seconds - (minutes * 60) - (hours * 60 * 60)
 	
-	local timestring = (hours and (hours .. 'h:')) .. (minutes and (minutes .. 'm:')) .. new_seconds .. 's'
+	local timestring = (hours ~= 0 and (hours .. ':') or '') .. ((minutes ~= 0 or hours ~= 0) and (minutes .. ':') or '') .. new_seconds
+	local timestring_long = hours .. 'h:' .. minutes .. 'm:' .. new_seconds .. 's'
 	
-	stack: get_meta(): set_string('description', description .. '\n' .. timestring)
+	local meta = stack: get_meta()
+	meta:set_int('count_alignment', 18)
+	meta:set_string('count_meta', timestring)
+	meta: set_string('description', description .. '\n' .. timestring_long)
 	return stack
 end
 
 etc: register_item('icon_chance', {
-	inventory_image = 'etc_icon_chance.png',
+	inventory_image = 'etc_icon_chance.png^etc_icon_bar_overlay.png',
 	groups = {not_in_creative_inventory = 1},
 	stack_max = -1
 })
@@ -39,20 +42,29 @@ function etc.get_chance_rep_stack (percent, addendum, conds)
 		condstring = condstring .. '\n\t* '..v
 	end
 	
-	percent = tostring(percent)
-	stack: get_meta(): set_string('description', 'Percent Chance\n'..percent: sub(1, math.min(5, #percent))..'%'..(addendum and addendum ~= '' and ' '..addendum)..condstring)
+	percentstr = tostring(percent)
+	
+	local meta = stack: get_meta()
+	meta:set_int('count_alignment', 18)
+	meta:set_string('count_meta', percentstr: sub(1, math.min(4, #percentstr))..'%')
+	meta: set_string('description', 'Percent Chance\n'..percentstr: sub(1, math.min(5, #percentstr))..'%'..(addendum and addendum ~= '' and ' '..addendum)..condstring)
 	return stack
 end
 
 etc: register_item('icon_power', {
-	inventory_image = 'etc_icon_power.png',
+	inventory_image = 'etc_icon_power.png^etc_icon_bar_overlay.png',
 	groups = {not_in_creative_inventory = 1},
 	stack_max = -1
 })
 
-function etc.get_power_rep_stack (description, unit, amount)
+function etc.get_power_rep_stack (description, unit, amount, image)
 	local stack = ItemStack('etcetera:icon_power')
-	stack: set_count(math.max(1, math.floor(amount)))
-	stack: get_meta(): set_string('description', 'Energy ('..description..')\n'..amount..unit)
+	local meta = stack: get_meta()
+	meta:set_int('count_alignment', 18)
+	meta:set_string('count_meta', amount..unit)
+	meta: set_string('description', 'Energy ('..description..')\n'..amount..unit)
+	if image then
+		meta: set_string('inventory_image', image..'^etc_icon_bar_overlay.png')
+	end
 	return stack
 end
